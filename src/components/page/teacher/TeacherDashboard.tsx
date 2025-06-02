6481f1b4c550511dc0a1bb9999c5c4bef809401f
mainakk58 <karmakarmainak.solution@gmail.com>
2025-06-02 13:42:49 +0530

    minor changes

--- src/components/page/teacher/TeacherDashboard.tsx
+++ src/components/page/teacher/TeacherDashboard.tsx
@@ -18,13 +18,21 @@ function TeacherDashboard() {
         <HumanIcon className="text-xl" />
         <DashboardItemName className="text-xl">Seller</DashboardItemName>
       </div>
-      <div>
-        <DashboardDropDown title="Seller">
-          <DashboardSubItem>
-            <HumanIcon className="text-xl" />
-            <DashboardItemName className="text-xl">Seller</DashboardItemName>
-          </DashboardSubItem>
-        </DashboardDropDown>
+      <div className="flex flex-col gap-3">
+          <DashboardDropDown title="Seller">
+            <DashboardSubItem>
+              <HumanIcon className="text-xl" />
+              <DashboardItemName className="text-xl">Seller</DashboardItemName>
+            </DashboardSubItem>
+            <DashboardSubItem>
+              <HumanIcon className="text-xl" />
+              <DashboardItemName className="text-xl">Seller</DashboardItemName>
+            </DashboardSubItem>
+            <DashboardSubItem>
+              <HumanIcon className="text-xl" />
+              <DashboardItemName className="text-xl">Seller</DashboardItemName>
+            </DashboardSubItem>
+          </DashboardDropDown>
       </div>
     </Dashboard>
   );
