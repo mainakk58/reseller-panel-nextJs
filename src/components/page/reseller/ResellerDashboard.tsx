1df1e03af09698e96433523942709b416ad69023
mainakk58 <karmakarmainak.solution@gmail.com>
2025-06-04 14:07:32 +0530

    reseller done

--- src/components/page/reseller/ResellerDashboard.tsx
+++ src/components/page/reseller/ResellerDashboard.tsx
@@ -12,19 +12,19 @@ function ResellerDashboard() {
         <p className="text-white text-lg font-semibold">Teacher Dashboard</p>
 
         <div className="w-full">
-          <Link href={"#"}>
+          <Link href={"/reseller/placeorder"}>
             <button className="flex items-center w-full gap-2 text-white px-2 py-2 hover:bg-blue-800 rounded transition-colors">
               <TruckIcon />
               Place Order
             </button>
           </Link>
-          <Link href={"#"}>
+          <Link href={"/reseller/yourorder"}>
             <button className="flex items-center w-full text-white px-2 py-2 hover:bg-blue-800 rounded transition-colors">
               <ShoppingIcon className="w-5 h-5 mr-2 text-white" />
               Your Orders
             </button>
           </Link>
-          <Link href={"#"}>
+          <Link href={"/reseller/wallet"}>
             <button className="flex items-center w-full text-white px-2 py-2 hover:bg-blue-800 rounded transition-colors">
               <WalletIcon className="w-5 h-5 mr-2 text-white" />
               Wallet
